79569a5f59e6a95d8907df9515f5ec0c7d9156df
Andre Marcos Gelhen <andre.gelhen@tozzoalimentos.com.br>
2025-10-16 13:41:55 -0300

    Primeiros componentes do streamlit

--- spotify.py
+++ spotify.py
@@ -1,2 +1,30 @@
 import streamlit as st
 import pandas as pd
+
+st.set_page_config(
+    layout="wide",
+    page_title="Spotify Songs"
+)
+
+df = pd.read_csv("01 Spotify.csv")
+
+df.set_index("Track", inplace=True)
+
+#Atista e quantas vezes apareceu com .value_counts() e para ser unico .index
+artists = df["Artist"].value_counts().index
+#recebe artista selecionado do listbox
+artist = st.selectbox("Artista", artists)
+df_filtered = df[df["Artist"] == artist]
+
+#faz outro filtro
+albuns = df_filtered["Album"].value_counts().index
+album = st.selectbox("Album", albuns)
+
+#novo filtro baseado no album
+df_filtered2 = df[df["Album"] == album]
+
+display = st.checkbox("Display")
+if display:
+    st.bar_chart(df_filtered2["Stream"])
+
+st.write(artist)
